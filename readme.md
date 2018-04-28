72b145ce120e854ee9ac442d6aa6841f7d733537
Steven <stevenxchung@gmail.com>
2018-04-28 19:38:48 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Sarae's Photography
+
+Sarae's Photography is a website designed to showcase Sarae's collection of photography and passion for traveling and exploring new places. This file will serve to track any major changes and updates to Sarae's website.
+
+## Sarae's Photography v1
+* Added landing page with scrolling image effect
---
+++ v1/app.js
@@ -0,0 +1,15 @@
+// Initialize Libraries
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.render("landing");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+  console.log("Server is live!");
+  console.log("PORT: " + process.env.PORT + " / " + "IP: " + process.env.IP);
+});
