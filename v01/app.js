5358a13d9e465586920286cf041450e929634ed4
Steven <stevenxchung@gmail.com>
2018-05-13 11:55:17 -0400

    Add routes to Gallery and Contact pages

--- v01/app.js
+++ v01/app.js
@@ -12,6 +12,14 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
+app.get("/gallery", function(req, res) {
+  res.render("gallery");
+})
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+})
+
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("Server is live!");
   console.log("PORT: " + process.env.PORT + " / " + "IP: " + process.env.IP);
