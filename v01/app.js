0c1a04271650997c1af9da22c605bd1ffb5859d8
Steven <stevenxchung@gmail.com>
2018-05-19 09:53:58 -0400

    Ditch AWS API initial commit

--- v01/app.js
+++ v01/app.js
@@ -38,6 +38,67 @@ app.get("/contact", function(req, res) {
   res.render("contact");
 });
 
+// POST - Contact form
+app.post("/contact", function(req, res) {
+
+});
+
+// POST - Password change route + user authentication
+router.post('/change', middleware.isLoggedIn, function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({}, function(err, user) {
+        if (!user) {
+          // Testing password change
+          console.log(user);
+          console.log(typeof user);
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+        if(req.body.password === req.body.confirm) {
+          user.setPassword(req.body.password, function(err) {
+
+            user.save(function(err) {
+              req.logIn(user, function(err) {
+                done(err, user);
+              });
+            });
+          })
+        } else {
+            req.flash("error", "Passwords do not match.");
+            return res.redirect('back');
+        }
+      });
+    },
+    // Drying code by using emailUser function
+    emailUser
+  ], function(err) {
+    req.flash('success', 'Success! Your password has been changed.');
+    res.redirect('/campgrounds');
+  });
+});
+
+function emailUser(user, done) {
+  var smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'aircamp.forgot@gmail.com',
+      pass: process.env.GMAILPW
+    }
+  });
+  var mailOptions = {
+    to: user.email,
+    from: 'aircamp.forgot@gmail.com',
+    subject: 'Your password has been changed',
+    text: 'Hello ' + user.userDisplay + ',\n\n' +
+      'This is a confirmation that the password for your account ' + user.email + ' on AirCamp has just been changed.\n'
+  };
+  smtpTransport.sendMail(mailOptions, function(err) {
+    done(err);
+  });
+  done();
+}
+
 app.listen(process.env.PORT, process.env.IP, function() {
   console.log("Server is live!");
   console.log("PORT: " + process.env.PORT + " / " + "IP: " + process.env.IP);
