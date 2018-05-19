591727a3f09e6f4fb1534eaba00f590207eb1055
Steven <stevenxchung@gmail.com>
2018-05-19 10:57:09 +0000

    Contact form capability with AWS API

---
+++ scripts/contact-api.js
@@ -0,0 +1,36 @@
+// Adds an event listener to our form. When the form is submitted, it will send data to our Lambda function, which in turn, will send us an email.
+document.getElementById('serverless-contact-form').addEventListener('submit', sendDataToLambda);
+
+// Now for the good stuff. This is the function that will send our data to AWS.
+function sendDataToLambda(e) {
+  e.preventDefault();
+
+  // Gets the values of each field in our form. This is the data we'll send to our Lambda function.
+  var formEmail = document.querySelector('.form-email').value;
+  var formSubject = document.querySelector('.form-subject').value;
+  var formMessage = document.querySelector('.form-message').value;
+
+  // This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
+  var endpoint = "https://g4nzsevhvi.execute-api.us-east-1.amazonaws.com/prod/ContactFormLambda";
+
+  // Remember those form values we just grabbed? We're going to put them into an object here.
+  var body = {
+    email: formEmail,
+    subject: formSubject,
+    message: formMessage
+  }
+
+  // Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
+  var lambdaRequest = new Request(endpoint, {
+    method: 'POST',
+    // Quick note: 'no-cors' mode is for development on localhost only!
+    mode: 'cors',
+    body: JSON.stringify(body)
+  });
+
+  // Call the Fetch API to make our request
+  fetch(lambdaRequest)
+    // This is where you can handle errors. This is just an example, so we won't cover that.
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
+}
