d3c318080f299f4b0385177a3f30b398790c0a3b
Steven <stevenxchung@gmail.com>
2023-06-07 16:32:47 -0400

    Move fade animation to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,19 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "fade-in-slow": "fade-in 4s ease-in-out",
+        "fade-in-medium": "fade-in 2s ease-in-out",
+        "fade-in-fast": "fade-in 1s ease-in-out",
+      },
       transitionDuration: {
         "2000": "2000ms",
+      },
+    },
+    keyframes: {
+      "fade-in": {
+        "0%": { opacity: "0" },
+        "100%": { opacity: "1" },
       },
     },
   },
